f497a975aaaf66f1d803cbcced00d5aa1cc30450
cjm <connorjamesmcmahon@gmail.com>
2025-05-10 22:00:42 +1200

    Add first test

--- src/powerful/power.py
+++ src/powerful/power.py
@@ -1,2 +1,19 @@
-def PowerMod(x, n, p):
-    return 1
+from tqdm import tqdm
+
+
+def power_mod(x, n, p):
+    binary = bin(n)
+    has_reached_one = False
+    m = x % p
+
+    for i in tqdm(binary[2 : len(binary)]):
+        if has_reached_one:
+            m = m**2 % p
+
+            if i == "1":
+                m = m * x % p
+
+        if not has_reached_one and i == "1":
+            has_reached_one = True
+
+    return m
--- tests/powerful/test_power.py
+++ tests/powerful/test_power.py
@@ -1,4 +1,4 @@
-from powerful.power import PowerMod
+from powerful.power import power_mod
 
 
 class TestPowerMod:
@@ -9,7 +9,43 @@ class TestPowerMod:
         c = 1
 
         # Act
-        result = PowerMod(a, b, c)
+        result = power_mod(a, b, c)
 
         # Assert
-        assert result == 1
+        assert result == 0
+
+    def test_mixed(self):
+        # Arrange
+        a = 2
+        b = 3
+        c = 4
+
+        # Act
+        result = power_mod(a, b, c)
+
+        # Assert
+        assert result == 0
+
+    def test_high_numbers(self):
+        # Arrange
+        a = 11111
+        b = 2345
+        c = 17
+
+        # Act
+        result = power_mod(a, b, c)
+
+        # Assert (tested with GAP)
+        assert result == 7
+
+    def test_arkadii_numbers(self):
+        # Arrange
+        a = 4919804113995030623
+        b = 4568442163565
+        c = 747528518238452134859
+
+        # Act
+        result = power_mod(a, b, c)
+
+        # assert
+        assert result == 711191711059966602217
